93289eabf5edbdc1fdeb3299fd8058fee6ae00cc
jacky0987 <adding2003@gmail.com>
2024-08-18 13:38:27 +0800

    basic folder structure generated for 1.20.4

---
+++ src/client/java/com/jacky0987/pixelgeneration/PixelGenerationClient.java
@@ -0,0 +1,10 @@
+package com.jacky0987.pixelgeneration;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PixelGenerationClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/jacky0987/pixelgeneration/PixelGenerationDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jacky0987.pixelgeneration;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PixelGenerationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
